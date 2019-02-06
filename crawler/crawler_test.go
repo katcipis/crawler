6f920081ca623198f1cbc402caa7fbe0b696044b
Tiago Katcipis <tiagokatcipis@gmail.com>
2019-02-06 14:52:15 -0200

    Create fake server

--- crawler/crawler_test.go
+++ crawler/crawler_test.go
@@ -1,6 +1,33 @@
 package crawler_test
 
-import "testing"
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"testing"
+)
 
-func TestCrawlerTextSitemap(t *testing.T) {
+func TestCrawler(t *testing.T) {
+	server, url := setupServer(t, "./testdata/fakesite")
+	defer server.Close()
+
+	fmt.Println("created test server", url)
+}
+
+func setupServer(t *testing.T, dir string) (*httptest.Server, url.URL) {
+	handler := http.FileServer(http.Dir(dir))
+	server := httptest.NewServer(handler)
+	url, err := url.Parse(server.URL)
+
+	fatalerr(t, err, "setting up server")
+	return server, *url
+}
+
+func fatalerr(t *testing.T, err error, op string) {
+	t.Helper()
+
+	if err != nil {
+		t.Fatalf("error[%s] while %s", err, op)
+	}
 }
